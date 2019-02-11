1367e64301dfaa21d9435e5010be6684377d0df4
Emroze <emrozesd@users.noreply.github.com>
2019-02-10 21:57:49 -0800

    added spotify info

--- liri.js
+++ liri.js
@@ -1,13 +1,30 @@
 
 require("dotenv").config();
 
+var keys = require("./keys.js");
+
 var axios = require("axios");
+
+var Spotify = require("node-spotify-api");
+
+
+
+var spotify = new Spotify(keys.spotify);
 
 var omddKey = "cf9c461b"
 
 var movieName = process.argv.slice(2); 
 
 var omdbURL = "http://www.omdbapi.com/?t=" + movieName + "=&plot=short&apikey=" + omddKey
+
+spotify
+  .search({ type: 'track', query: 'Smile' })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
 axios.get(omdbURL).then(
   function(response) {
