80303c1db7fb80bfcfe21e0bead0b29b604cce3f
Emroze <emrozesd@users.noreply.github.com>
2019-02-11 19:48:58 -0800

    wrote concert logic

--- liri.js
+++ liri.js
@@ -5,16 +5,65 @@ var keys = require("./keys.js");
 
 var axios = require("axios");
 
+var moment = require("moment");
+
 var Spotify = require("node-spotify-api");
-
-
 
 var spotify = new Spotify(keys.spotify);
 
 var omddKey = "cf9c461b"
 
-var movieName = process.argv.slice(2); 
+var command = process.argv[2]; 
 
+if (command === "concert-this") {
+  findBand();
+}
+else if (command === "spotify-this-song") {
+  findSong();
+}
+else if (command === "movie-this") {
+  findMovie();
+}
+else if (command === "do-what-it-says") {
+  findText();
+}
+else {
+  console.log("Invalid command.  Try again")
+}
+
+function findBand() {
+
+  var bandName = process.argv.slice(3).join(" ");
+  var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp"
+
+    axios.get(queryURL).then(
+      function(response) {
+
+          for (var i = 0; i < response.data.length; i++) {
+
+            var date = moment(response.data[i].datetime).format("MM/DD/YYYY")
+
+            var venueObj = {
+              name: response.data[i].venue.name,
+              country: response.data[i].venue.country,
+              state: response.data[i].venue.region,
+              city: response.data[i].venue.city,
+              date: date
+            };
+
+            console.log(venueObj);                       
+
+          }
+    
+    });
+}
+      
+
+
+
+
+
+/*
 var omdbURL = "http://www.omdbapi.com/?t=" + movieName + "=&plot=short&apikey=" + omddKey
 
 spotify
@@ -31,3 +80,4 @@ axios.get(omdbURL).then(
     console.log(response.data);
   }
 );
+*/
